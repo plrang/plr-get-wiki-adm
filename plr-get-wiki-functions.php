f138d5d4347db5b309754e00a145ef6cfabdcbaf
plrang <android@rang.pl>
2018-10-31 18:58:36 +0100

    FIX folding the snippet input fields

--- plr-get-wiki-functions.php
+++ plr-get-wiki-functions.php
@@ -16,11 +16,16 @@ function plrWiki_in_post_edit_display()
 }
 
 function plrWiki_myfunction() { 
+    
+    // Main Wikipedia fetch/renderer
     wp_register_script ( 'plr-wiki-area-render' , plugins_url('/js/wiki-summary-render.js', __FILE__) , array('jquery')); // this last part-( array('jquery') )is added in case your script needs to be included after jquery
     wp_localize_script( 'plr-wiki-area-render', 'plrAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));  
     wp_enqueue_script ( 'plr-wiki-area-render', plugins_url('/js/wiki-summary-render.js', __FILE__) , array('jquery'), '1.3', true  ); // then print. it will be added after jquery is added
     }            
-
-
+    
+    // Additional JS functions
+    wp_register_script ( 'plr-funops' , plugins_url('/js/modules/funops.js', __FILE__) , array('jquery')); // this last part-( array('jquery') )is added in case your script needs to be included after jquery
+    wp_localize_script( 'plr-funops', 'plrAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));  
+    wp_enqueue_script ( 'plr-funops', plugins_url('/js/modules/funops.js', __FILE__) , array('jquery'), '1.3', true  ); // then print. it will be added after jquery is added
 
 ?>

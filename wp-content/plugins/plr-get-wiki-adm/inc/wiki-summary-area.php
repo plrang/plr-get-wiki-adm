98d1cbd9533516940e26b2fadffb0bf8896d5fb5
plrang <android@rang.pl>
2018-11-02 23:05:09 +0100

    ADD plugin autoupdate | 2nd check

--- wp-content/plugins/plr-get-wiki-adm/inc/wiki-summary-area.php
+++
@@ -1,4 +0,0 @@
-<section id="plr-wiki-area-js">
-<span class="plrwiki-item-summary">"{{SUMMARY}}"</span>
-<span class="plrwiki-item-url">Wikipedia, The Free Encyclopedia, <b class="plrwiki-item-title">"{{TITLE}}"</b>, {{URL}}</span>
-</section>
--- wp-content/plugins/plr-get-wiki-adm/plr_get_wiki_adm_widget.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Get Wiki - Get This Wikipedia Summary
-// Plrang Art 2018
-// Not yet in use
-?>
